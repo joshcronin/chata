5ae0f7af947450a760cb6ea96d235cf7a4254a23
Bradley Birch <Bradley.Birch@ig.com>
2016-10-31 10:10:01 +0000

    Added Firebase config

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,3 @@
-console.log('\'Allo \'Allo!');
+// Define a new module for chata app. The array holds the names of dependencies if any.
+// $firebaseObject, $firebaseArray, and $firebaseAuth can be injected into any controller, service, or factory.
+var app = angular.module("chataApp", ["firebase"]);

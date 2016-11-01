93be1db24f92ed4eb32e4b6976944c434e78be89
Josh Cronin <jcronin@positive.co.uk>
2016-11-01 12:05:57 +0000

    Added JavaScript to grab form data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,36 @@ app.run(['Pubnub', function (Pubnub) {
         subscribeKey: 'sub-c-8ebd3efa-9f52-11e6-aff8-0619f8945a4f'
     });
 }]);
+
+app.controller("LoginController", function($scope) {
+
+	//Error content
+	$scope.error = {
+		message: "Username or password incorrect",//Message to be shown in form
+		show: false//If true, message will be visible in form
+	};
+   
+	$scope.usr_email = '';//The email field value
+	$scope.usr_password = '';//The password field value
+
+	//Method called on form submit
+	$scope.submit = function() {
+		console.group('Login submission');
+		console.log('Username: ' + $scope.usr_email);
+		console.log('Password: ' + $scope.usr_password);
+		console.groupEnd();
+		//If there is an error use this method
+		$scope.throwError();
+	};
+
+	//Method to throw error
+	$scope.throwError = function() {
+		$scope.error.show = true;
+	};
+
+	//Method to throw error
+	$scope.cancelError = function() {
+		$scope.error.show = false;
+	};
+
+});

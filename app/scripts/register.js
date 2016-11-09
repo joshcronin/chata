d6a356b3393b569a9bcf188024e938bd7768d2f4
Bradley Birch <Bradley.Birch@ig.com>
2016-11-09 11:41:50 +0000

    Can register and log in

--- app/scripts/register.js
+++ app/scripts/register.js
@@ -5,6 +5,11 @@ var app = angular.module("chataApp", ["firebase"]);
 
 app.controller("registerController", function($scope, $firebaseAuth) {
 
+  $scope.error = {
+    message: "", //Message to be shown in form
+    show: false //If true, message will be visible in form
+  };
+
   $scope.user = {
     email: '', //The email field value
     password: '', //The password field value
@@ -13,7 +18,33 @@ app.controller("registerController", function($scope, $firebaseAuth) {
 
   //Method called on form submit
   $scope.submit = function() {
+    $firebaseAuth().$createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
+      .then(function(firebaseUser) {
+        var ref = firebase.database().ref();
+        ref.child("users").child(firebaseUser.uid).set({
+          name: $scope.user.username
+        });
+      })
+      .then(function(firebaseUser) {
+        $firebaseAuth().$signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(firebaseUser) {
+          // Temporary before we have authenticated routes
+          console.log("Signed in as:", firebaseUser.uid);
+        });
+      })
+      .catch(function(error) {
+        $scope.throwError(error.message);
+      });
+  };
 
+  //Method to throw error
+  $scope.throwError = function(error) {
+    $scope.error.message = error || 'Something went wrong. Please try again';
+    $scope.error.show = true;
+  };
+
+  //Method to cancel error
+  $scope.cancelError = function() {
+    $scope.error.show = false;
   };
 });
 

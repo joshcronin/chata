9434616ef19627f30442b3bd7f2b5a58f73e2980
Bradley Birch <Bradley.Birch@ig.com>
2016-11-16 10:52:54 +0000

    pull out directive to new file

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -133,28 +133,3 @@ registerController.controller("register", ['$firebaseAuth', '$scope', '$location
     return false;
   };
 }]);
-
-/**
- * Directive that will compare two input fields
- *
- */
-var compareTo = function() {
-  return {
-    require: "ngModel",
-    scope: {
-      otherModelValue: "=compareTo"
-    },
-    link: function(scope, element, attributes, ngModel) {
-
-      ngModel.$validators.compareTo = function(modelValue) {
-        return modelValue == scope.otherModelValue;
-      };
-
-      scope.$watch("otherModelValue", function() {
-        ngModel.$validate();
-      });
-    }
-  };
-};
-
-chataApp.directive("compareTo", compareTo);
---
+++ app/scripts/directives/compareTo.js
@@ -0,0 +1,24 @@
+/**
+ * Directive that will compare two input fields
+ *
+ */
+var compareTo = function() {
+  return {
+    require: "ngModel",
+    scope: {
+      otherModelValue: "=compareTo"
+    },
+    link: function(scope, element, attributes, ngModel) {
+
+      ngModel.$validators.compareTo = function(modelValue) {
+        return modelValue == scope.otherModelValue;
+      };
+
+      scope.$watch("otherModelValue", function() {
+        ngModel.$validate();
+      });
+    }
+  };
+};
+
+chataApp.directive("compareTo", compareTo);

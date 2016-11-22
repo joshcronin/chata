ac2a2779e8a679b5c5e48b3660b446abfd5b29a5
Matthew Fergusson-White <Matthew.Fergusson-White@ig.com>
2016-11-22 11:59:30 +0000

    set username and email

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,5 +2,16 @@ var profileController = angular.module('profileController', []);
 
 profileController.controller("profile", ['$firebaseAuth', '$scope', '$location', '$http',
   function($firebaseAuth, $scope, $location, $http) {
+    $scope.user = {
+      email: $firebaseAuth().$getAuth().email,
+      username: $scope.getUsername,
+    };
+
+    $scope.getUsername = function (){
+      var ref = firebase.database().ref();
+      ref.child("/users/" + $firebaseAuth().$getAuth().uid + '/name/').once('value').then(function(snap){
+        $scope.username = snap.val());
+      });
+    }
   }
 ]);

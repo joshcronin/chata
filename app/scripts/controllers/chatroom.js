e6c4bd68d4f2f83988f10f0d61e6b1d2a42ecb79
SilverBirchh <bradleybirch09@yahoo.co.uk>
2016-11-22 11:05:51 +0000

    Messages work

--- app/scripts/controllers/chatroom.js
+++ app/scripts/controllers/chatroom.js
@@ -1,49 +1,47 @@
 var chatController = angular.module('chatController', []);
 
-chatController.controller("chat", ['Pubnub', '$firebaseAuth', '$scope', '$location', '$http',
-  function(Pubnub, $firebaseAuth, $scope, $location, $http) {
+chatController.controller("chat", ['Pubnub', '$pubnubChannel', '$firebaseAuth', '$scope', '$location', '$http',
+  function(Pubnub, $pubnubChannel, $firebaseAuth, $scope, $location, $http) {
 
-    Pubnub.addListener({
-      status: function(statusEvent) {
-        if (statusEvent.category === "PNUnknownCategory") {
-          var newState = {
-            new: 'error'
-          };
-          Pubnub.setState({
-              state: newState
-            },
-            function(status) {
-              console.log(statusEvent.errorData.message)
-            }
-          );
-        }
-      },
-      message: function(message) {
-        console.log(message)
-      }
-    })
-
-    Pubnub.subscribe({
-      channels: ['chata']
+    $scope.messageList = $pubnubChannel('chata', {
+      autoload: 0
     });
 
     $scope.message = ''; // Users message
+
+    $scope.error = {
+      message: "", //Message to be shown in form
+      show: false //If true, message will be visible in form
+    };
+
     $scope.publish = function() {
+      $scope.cancelError();
       Pubnub.publish({
           message: {
-            such: $scope.message
+            content: $scope.message
           },
           channel: 'chata'
         },
         function(status, response) {
           if (status.error) {
             // handle error
-            console.log(status)
+            $scope.throwError(error.message);
           } else {
-            console.log("message Published w/ timetoken", response.timetoken)
+            $scope.message = '';
           }
         }
       );
     }
+
+    //Method to throw error
+    $scope.throwError = function(error) {
+      $scope.error.message = error || 'Something went wrong. Please try again';
+      $scope.error.show = true;
+    };
+
+    //Method to cancel error
+    $scope.cancelError = function() {
+      $scope.error.show = false;
+    };
   }
 ]);

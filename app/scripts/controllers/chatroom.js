5e1aa34dc291a73ccd7f2257947a6d917bb8b85a
SilverBirchh <bradleybirch09@yahoo.co.uk>
2016-11-22 10:45:45 +0000

    Sending receiving basic message

--- app/scripts/controllers/chatroom.js
+++ app/scripts/controllers/chatroom.js
@@ -1,6 +1,49 @@
 var chatController = angular.module('chatController', []);
 
-chatController.controller("chat", ['$firebaseAuth', '$scope', '$location', '$http',
-  function($firebaseAuth, $scope, $location, $http) {
+chatController.controller("chat", ['Pubnub', '$firebaseAuth', '$scope', '$location', '$http',
+  function(Pubnub, $firebaseAuth, $scope, $location, $http) {
+
+    Pubnub.addListener({
+      status: function(statusEvent) {
+        if (statusEvent.category === "PNUnknownCategory") {
+          var newState = {
+            new: 'error'
+          };
+          Pubnub.setState({
+              state: newState
+            },
+            function(status) {
+              console.log(statusEvent.errorData.message)
+            }
+          );
+        }
+      },
+      message: function(message) {
+        console.log(message)
+      }
+    })
+
+    Pubnub.subscribe({
+      channels: ['chata']
+    });
+
+    $scope.message = ''; // Users message
+    $scope.publish = function() {
+      Pubnub.publish({
+          message: {
+            such: $scope.message
+          },
+          channel: 'chata'
+        },
+        function(status, response) {
+          if (status.error) {
+            // handle error
+            console.log(status)
+          } else {
+            console.log("message Published w/ timetoken", response.timetoken)
+          }
+        }
+      );
+    }
   }
 ]);

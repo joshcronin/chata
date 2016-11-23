1b185fa2dca6bc58326c9860fe1998a247318f85
tanyastittle <tanya@bango.com>
2016-11-23 10:40:25 +0000

    further ui and validation

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,8 +2,30 @@ var profileController = angular.module('profileController', []);
 
 profileController.controller("profile", ['$firebaseAuth', '$scope', '$location', '$http',
   function($firebaseAuth, $scope, $location, $http) {
+	$scope.error = {
+    message: "", //Message to be shown in form
+    show: false //If true, message will be visible in form
+  };
+
+  $scope.changePassword = {
+    newPass: '', //The newPass field value
+    conPass: '' //The conPass field value
+  };
+   //Method to throw error
+  $scope.throwError = function(error) {
+    $scope.error.message = error || 'Something went wrong. Please try again';
+    $scope.error.show = true;
+  };
+
+  //Method to cancel error
+  $scope.cancelError = function() {
+    $scope.error.show = false;
+  };
+  
   }
 ]);
+
+
  // Toggle the visibility of the password form
  function togglePasswordForm() {
 	 if(document.getElementById('hidden1').style.display == 'block')

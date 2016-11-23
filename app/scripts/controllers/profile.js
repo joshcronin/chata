eecd9596e502b73bedfd6d342bf3325dd2b36c1b
tanyastittle <tanya@bango.com>
2016-11-23 09:05:40 +0000

    UI changes

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -4,3 +4,17 @@ profileController.controller("profile", ['$firebaseAuth', '$scope', '$location',
   function($firebaseAuth, $scope, $location, $http) {
   }
 ]);
+ // Toggle the visibility of the password form
+ function togglePasswordForm() {
+	 if(document.getElementById('hidden1').style.display == 'block')
+    {
+	
+	document.getElementById('hidden1').style.display = "none";
+	document.getElementById('passwordToggle').textContent = "Change";
+	} 
+	else {
+		document.getElementById('hidden1').style.display = "block";
+		document.getElementById('passwordToggle').textContent = "Cancel";
+	}
+	
+};
